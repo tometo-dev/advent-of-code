fff3de590a6f22ae430a0a5b95f3471e3e59f9f8
Sudhanshu <perfectsudh@gmail.com>
2022-12-03 19:22:46 +0530

    Solve part 1

---
+++ 2022/rust/day-03/src/lib.rs
@@ -0,0 +1,42 @@
+use std::collections::HashMap;
+
+pub fn process_part1(input: &str) -> usize {
+    let letter_scores = ('a'..='z')
+        .chain('A'..='Z')
+        .enumerate()
+        .map(|(idx, c)| (c, idx + 1))
+        .collect::<HashMap<char, usize>>();
+
+    let result = input
+        .lines()
+        .map(|rucksack| {
+            let len = rucksack.len() / 2;
+            let compartment_1 = &rucksack[..len];
+            let compartment_2 = &rucksack[len..];
+
+            let common_item = compartment_1
+                .chars()
+                .find(|c| compartment_2.contains(*c))
+                .unwrap();
+            letter_scores.get(&common_item).unwrap()
+        })
+        .sum::<usize>();
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_works() {
+        const INPUT: &str = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+        let result = process_part1(&INPUT);
+        assert_eq!(result, 157);
+    }
+}
