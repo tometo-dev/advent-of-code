7ab4a2ac351b465bbdd1fc629413671d6be3c7df
Sudhanshu <perfectsudh@gmail.com>
2022-12-03 19:54:40 +0530

    Solve part 2 after enabling nightly feature

--- 2022/rust/day-03/src/lib.rs
+++ 2022/rust/day-03/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(iter_array_chunks)]
+
 use std::collections::HashMap;
 
 pub fn process_part1(input: &str) -> usize {
@@ -24,6 +26,29 @@ pub fn process_part1(input: &str) -> usize {
     result
 }
 
+pub fn process_part2(input: &str) -> usize {
+    let letter_scores = ('a'..='z')
+        .chain('A'..='Z')
+        .enumerate()
+        .map(|(idx, c)| (c, idx + 1))
+        .collect::<HashMap<char, usize>>();
+
+    let result = input
+        .lines()
+        .array_chunks::<3>()
+        .map(|[a, b, c]| {
+            let common_item = a
+                .chars()
+                .find(|a_char| b.contains(*a_char) && c.contains(*a_char))
+                .unwrap();
+
+            letter_scores.get(&common_item).unwrap()
+        })
+        .sum::<usize>();
+
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -38,5 +63,17 @@ ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw";
         let result = process_part1(&INPUT);
         assert_eq!(result, 157);
+    }
+
+    #[test]
+    fn part2_works() {
+        const INPUT: &str = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+        let result = process_part2(&INPUT);
+        assert_eq!(result, 70);
     }
 }
