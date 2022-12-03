861ecaacc7e304ba70bf77b5f125d0819083bcd2
Sudhanshu <perfectsudh@gmail.com>
2022-12-03 17:26:00 +0530

    Solve part 2

--- 2022/rust/day-01/src/lib.rs
+++ 2022/rust/day-01/src/lib.rs
@@ -13,13 +13,28 @@ pub fn process_part1(input: &str) -> String {
     result.to_string()
 }
 
+pub fn process_part2(input: &str) -> String {
+    let mut result = input
+        .split("\n\n")
+        .map(|elf_calories| {
+            elf_calories
+                .lines()
+                .map(|item| item.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .collect::<Vec<u32>>();
+
+    result.sort_by(|a, b| b.cmp(a));
+    let result = result.iter().take(3).sum::<u32>();
+
+    result.to_string()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn part1_works() {
-        let input = "1000
+    const INPUT: &str = "1000
 2000
 3000
 
@@ -33,7 +48,16 @@ mod tests {
 9000
 
 10000";
-        let result = process_part1(&input);
+
+    #[test]
+    fn part1_works() {
+        let result = process_part1(&INPUT);
         assert_eq!(result, "24000");
+    }
+
+    #[test]
+    fn part2_works() {
+        let result = process_part2(&INPUT);
+        assert_eq!(result, "45000");
     }
 }
